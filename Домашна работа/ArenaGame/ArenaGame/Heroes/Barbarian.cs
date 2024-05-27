640093481e0096dc2b408e892bc2a2aba2483083
PetarPetrov04 <petar.004.petrov@gmail.com>
2024-05-27 12:25:22 +0300

    Remove folder from repo

---
+++ Домашна работа/ArenaGame/ArenaGame/Heroes/Barbarian.cs
@@ -0,0 +1,30 @@
+﻿using ArenaGame.Weapons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Barbarian : Hero
+    {
+        private bool isBerserkModeActive = false;
+        public Barbarian(string name, double armor, double strenght, IWeapon weapon) 
+            : base(name, armor, strenght, weapon)
+        {
+        }
+
+        public override double Attack()
+        {
+            double damage = base.Attack();
+
+            if(this.Health <= 40 && isBerserkModeActive == false) //Battle Axe ability
+            {
+                damage += damage * 2;
+            }
+
+            return damage;
+        }
+    }
+}
